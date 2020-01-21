b9021d92c824b2b2f151921b7df43beb7ec0a3da
Richard_Amankwaa <ramankwaa@college.harvard.edu>
2020-01-21 14:55:59 -0500

    trying to control the search

--- src/containers/MainPage/Search/SearchBar/index.js
+++ src/containers/MainPage/Search/SearchBar/index.js
@@ -1,1 +1,27 @@
 import React from 'react'
+import { Container } from './styles'
+
+
+const SearchBar = () => (
+  <Container>
+    <form method="get" action="http://www.google.com/search">
+
+
+      <input
+        type="text"
+        name="q"
+        size="25"
+        maxLength="255"
+        value="Google site search"
+        onFocus="if(this.value==this.defaultValue)this.value=''; this.style.color='black';"
+        onBlur="if(this.value=='')this.value=this.defaultValue; "
+      />
+      <input type="submit" value="Go!" />
+      <input type="hidden" name="sitesearch" value="yoursite.com" />
+
+
+    </form>
+  </Container>
+)
+
+export default SearchBar
--- src/containers/Welcome/index.js
+++ src/containers/Welcome/index.js
@@ -1,19 +1,22 @@
 import React from 'react'
-import {Container} from './styles'
+import { Container } from './styles'
 import LoginLink from '../components/LoginLink'
 import RegisterLink from '../components/RegisterLink'
 
 
-const Welcome = () => {
+const Welcome = () => (
+  <Container>
+    <h1>Welcome to Your Dream Page!</h1>
+    <p>
+Words!
+            More Words!!
+            words?
+    </p>
+    <LoginLink />
+    <RegisterLink />
 
-    return <Container>
-        <h1>Welcome to Your Dream Page!</h1>
-        <p>Words!<br>More Words!!</br>words?</p>
-        <LoginLink />
-        <RegisterLink />
-
-    </Container>
-}
+  </Container>
+)
 
 
 export default Welcome
