0ecc02e0918a33dc1f872ef491cb418412148ec2
KevinH48264 <1kevin.huang@gmail.com>
2020-01-21 16:18:09 -0500

    Peter's Weather API added

--- src/containers/Welcome/index.js
+++ src/containers/Welcome/index.js
@@ -1,9 +1,15 @@
+<<<<<<< HEAD
 import React from 'react'
 import { Container } from './styles'
+=======
+import React, { useState, useEffect } from 'react'
+import {Container} from './styles'
+>>>>>>> cdaf0b6... Peter's Weather API added
 import LoginLink from '../components/LoginLink'
 import RegisterLink from '../components/RegisterLink'
 
 
+<<<<<<< HEAD
 const Welcome = () => (
   <Container>
     <h1>Welcome to Your Dream Page!</h1>
@@ -14,6 +20,29 @@ const Welcome = () => (
 
     <LoginLink />
     <RegisterLink />
+=======
+const Welcome = () => {
+    const [weather, setWeather] = useState()
+    useEffect(() => {
+        if (!weather) {
+          navigator.geolocation.getCurrentPosition(async ({ coords }) => {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coords.latitude}&lon=${coords.longitude}&appid=db5bbba816b58757082ce2230c7754a6&units=imperial`)
+            const data = await response.json()
+            setWeather(data)
+          // eslint-disable-next-line no-alert
+          }, () => alert('Failed to fetch weather data.'))
+        }
+      }, [weather])
+
+    return <Container>
+        <h1>Welcome to Your Dream Page!</h1>
+<p>Words!words?</p>
+        <LoginLink />
+        <RegisterLink />
+        {weather ? <p>City: {weather.name} Weather: {weather.weather[0].description}</p> : <p>Your weather data is loading...</p>}
+    </Container>
+}
+>>>>>>> cdaf0b6... Peter's Weather API added
 
   </Container>
 )
