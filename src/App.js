04ad9688b9c8c3b49cd85c2d1df4d9ad0e54d20c
Richard_Amankwaa <ramankwaa@college.harvard.edu>
2020-01-22 10:16:21 -0500

    fixed some conflicts I found

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import Register from './containers/Register'
 import MainPage from './containers/MainPage'
 
 
-<<<<<<< HEAD
 const App = () => (
   <Router>
     <ThemeProvider theme={theme}>
@@ -27,25 +26,6 @@ const App = () => (
     </ThemeProvider>
   </Router>
 )
-=======
-const App = () => {
-  return (
-    <Router>
-      <ThemeProvider theme={theme}>
-        <ApolloProvider client={client}>
-          <div className="App">
-            <Switch>
-              <Route path="/login" component={Login} />
-              <Route path="/register" component={Register} />
-              <Route path="/home" component={MainPage} />
-              <Route path="/" component={Welcome} />
-            </Switch>
-          </div>
-        </ApolloProvider>
-      </ThemeProvider>
-    </Router>
-  )
-}
->>>>>>> cdaf0b6... Peter's Weather API added
+
 
 export default App
--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -4,33 +4,34 @@ import { useMutation } from '@apollo/react-hooks'
 import { LOGIN } from '../graphql'
 
 const Login = () => {
-    const history = useHistory()
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [login, { loading, data, error, called }] = useMutation(LOGIN, {
-        variables: {
-            email: email,
-            password: password,
-        },
-        onCompleted: ({ login: { token } }) => localStorage.setItem('token', token),
-    })
-
+  const history = useHistory()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [login, {
+    loading, data, error, called,
+  }] = useMutation(LOGIN, {
+    variables: {
+      email,
+      password,
+    },
+    onCompleted: ({ login: { token } }) => localStorage.setItem('token', token),
   })
+
 
   if (loading) return <p> Loading ... </p>
   if (error) {
     return (<p>Error</p>)
   }
 
-    return(
-        <div>
-            <p>Login Page</p>
-            <input type="text" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
-            <input type="text" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
-            <button type="button" onClick={login}>Login</button>
-            {called ? history.push("/home") : <p>NOT CALLED</p>}
-        </div>
-    )
+  return (
+    <div>
+      <p>Login Page</p>
+      <input type="text" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
+      <input type="text" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+      <button type="button" onClick={login}>Login</button>
+      {called ? history.push('/home') : <p>NOT CALLED</p>}
+    </div>
+  )
 }
 
 export default Login
