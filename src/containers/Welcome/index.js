b3a906f261f33c26132c99dd7c3c5ec6e498fe79
Richard_Amankwaa <ramankwaa@college.harvard.edu>
2020-01-23 14:43:45 -0500

    figuring out logout thing

--- src/containers/Welcome/index.js
+++ src/containers/Welcome/index.js
@@ -16,22 +16,23 @@ const Welcome = () => {
 
   return (
     <div>
-      <Image
-        src="https://cdn.wallpapersafari.com/64/53/DI52GS.jpg"
-        alt="weather"
-        fade={fade}
-      />
-      <WelcomeDiv fade={fade}>
-        <Header>Introducing Your Dream Page!</Header>
-        <p style={{
-          margin: '10px 30px', textAlign: 'center', fontSize: '15px', display: 'flex', justifyContent: 'center', alignItems: 'center',
-        }}
-        >
+      <Container>
+        <Image
+          src="https://cdn.wallpapersafari.com/64/53/DI52GS.jpg"
+          alt="weather"
+          fade={fade}
+        />
+        <WelcomeDiv fade={fade}>
+          <Header>Introducing Your Dream Page!</Header>
+          <p style={{
+            margin: '10px 30px', textAlign: 'center', fontSize: '15px', display: 'flex', justifyContent: 'center', alignItems: 'center',
+          }}
+          >
 This is a build-your-own homepage where you can add and remove features to your liking. You can login or register below!
-        </p>
-        <LoginLink />
-        <RegisterLink />
-      </WelcomeDiv>
+          </p>
+          <LoginLink />
+          <RegisterLink />
+        </WelcomeDiv>
 
       </Container>
     </div>
